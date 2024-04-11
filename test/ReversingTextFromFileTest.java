42b67266fff8d534eddfd945f0418f340509a232
Stanislaw Malewicz <szeliga71@wp.pl>
2024-04-11 16:40:55 +0200

    11.4.2024 /5

--- test/ReversingTextFromFileTest.java
+++ test/ReversingTextFromFileTest.java
@@ -1,66 +1,67 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
 
 public class ReversingTextFromFileTest {
 
+    String textFromFile = "";
+    StringBuilder textBuilder = new StringBuilder();
+    String text1 = "Test pliku testowego w nowym katalogu o nazwie testFiles.";
 
-    @Test
 
-    public void reverseMethodTest() {
+    @BeforeEach
+
+    public void create() {
         String directoryPath = "testFiles";
-
         File directory = new File(directoryPath);
 
-        if(!directory.exists()){
+        if (!directory.exists()) {
 
-            boolean created=directory.mkdir();
+            boolean created = directory.mkdir();
 
-        if(created){
-            System.out.println(" utworzno katalog");}
-        else{
-            System.out.println(" nie udalo sie utworzyc katalogu ");
+            if (created) {
+                System.out.println(" utworzno katalog");
+            } else {
+                System.out.println(" nie udalo sie utworzyc katalogu ");
 
-        }
-        }else{
+            }
+        } else {
             System.out.println(" katalog juz istnieje ");
         }
 
-        File testFile1=new File("testFiles/testFile1");
+        File testFile1 = new File("testFiles/testFile1");
 
-        String text1="Test pliku testowego w nowym katalogu o nazwie testFiles.";
 
-        try(FileWriter fileWriter=new FileWriter(testFile1);BufferedWriter bufferedWriter=new BufferedWriter(fileWriter);){
+
+        try (FileWriter fileWriter = new FileWriter(testFile1); BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);) {
 
             bufferedWriter.write(text1);
             bufferedWriter.flush();
-
-        }catch(IOException ioException){
-            ioException.printStackTrace();
-        }
-
-
-        String textFromFile="";
-        StringBuilder textBuilder=new StringBuilder();
-
-
-
-        try (FileReader fileReader = new FileReader(testFile1);BufferedReader bufferedReader = new BufferedReader(fileReader);){
-
-            textFromFile=bufferedReader.readLine();
-            while(textFromFile!=null){
-                textBuilder.append(textFromFile);
-                textFromFile=bufferedReader.readLine();
-            }
-
 
         } catch (IOException ioException) {
             ioException.printStackTrace();
         }
 
-        Assertions.assertEquals(textBuilder.toString(),text1);
+        try (FileReader fileReader = new FileReader(testFile1); BufferedReader bufferedReader = new BufferedReader(fileReader);) {
 
+            textFromFile = bufferedReader.readLine();
+            while (textFromFile != null) {
+                textBuilder.append(textFromFile);
+                textFromFile = bufferedReader.readLine();
+            }
+
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+    }
+
+    @Test
+
+    public void reverseMethodTest() {
+
+        Assertions.assertEquals(textBuilder.toString(), text1);
 
 
     }
